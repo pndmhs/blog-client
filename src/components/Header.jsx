a09794beab733288d87e5859878608e048678f58
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-15 22:30:40 +0700

    Add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full flex items-center py-4 px-5">
+      <div>
+        <h1 className="text-xl font-semibold">Simple Blog</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
