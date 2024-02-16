018b12a8dd99cd2627542b31faf1c0de19fadf82
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-16 08:32:06 +0700

    Change header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className="w-full flex items-center py-4 px-5">
-      <div>
+    <header className="w-full flex items-center py-4 lg:py-6 px-5">
+      <div className="w-full max-w-[820px] mx-auto">
         <h1 className="text-xl font-semibold">Simple Blog</h1>
       </div>
     </header>
