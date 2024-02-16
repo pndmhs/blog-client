471a28398280529934e88df01464988c3b1060a9
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 06:19:01 +0700

    Add comment list component below the post

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,3 +1,5 @@
+import CommentLists from "./CommentsList";
+
 const text = `Astrophotography opens a portal to the vast and awe-inspiring cosmos, allowing us to capture the beauty of distant galaxies and celestial bodies. To embark on this journey, start by investing in a sturdy tripod and a DSLR or mirrorless camera with manual settings. Understanding the importance of location and timing is crucial; choose a dark sky area, away from city lights, and shoot during the new moon phase for optimal conditions.
 
 Additionally, familiarize yourself with the basic principles of astrophotography, such as long exposure techniques and aperture settings, to capture the faint light emitted by distant stars and nebulae. Patience is key in astrophotography, as capturing stunning celestial images often requires long exposure times, sometimes lasting several minutes. During these extended exposures, it's essential to minimize any vibrations or movement, so using a remote shutter release or the camera's built-in timer can help ensure sharp and clear images.
@@ -14,7 +16,7 @@ const PostDetail = () => {
           Unveiling the Wonders of Astrophotography: A Beginner's Guide
         </h2>
         <p className="text-yellow-900">11 Feb, 2024</p>
-        <div className="mt-8 flex flex-col gap-6 font-merriweather">
+        <div className="py-8 flex flex-col gap-6 font-merriweather">
           {text
             .split("\n")
             .filter((par) => par !== "")
@@ -22,6 +24,8 @@ const PostDetail = () => {
               <p key={index}>{par}</p>
             ))}
         </div>
+        <hr />
+        <CommentLists />
       </div>
     </main>
   );
