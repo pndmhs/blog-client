719bebfa33b89a9e0b4f59520b9f91133438baae
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 06:54:20 +0700

    Add margin on the comment text

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -3,7 +3,7 @@ const CommentItem = ({ name, timestamp, text }) => {
     <div className="py-4 px-5 border border-solid border-neutral-400">
       <h4 className="font-medium text-lg">{name}</h4>
       <p className="text-yellow-900 text-sm">{timestamp}</p>
-      <p className="mt-2">{text}</p>
+      <p className="mt-3">{text}</p>
     </div>
   );
 };
