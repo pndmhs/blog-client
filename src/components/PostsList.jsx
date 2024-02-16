dfa9560d8af2b1d04144ea576a7f7a79739e8c61
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-16 08:31:44 +0700

    Add posts list component

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,12 @@
+const PostsList = () => {
+  return (
+    <main className="w-full px-5">
+      <div className="w-full max-w-[820px] mx-auto">
+        <h2 className="text-4xl font-semibold mt-8">Posts</h2>
+        <p className="opacity-55">3 Posts</p>
+      </div>
+    </main>
+  );
+};
+
+export default PostsList;
