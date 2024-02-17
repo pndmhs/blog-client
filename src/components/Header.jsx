a6a9eaa9c5e67173a49970c19e764bd2b97c645c
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 12:00:40 +0700

    Add home and login link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 const Header = () => {
   return (
     <header className="w-full flex items-center py-4 lg:py-6 px-5">
-      <div className="w-full max-w-[820px] mx-auto">
-        <h1 className="text-xl font-semibold">Simple Blog</h1>
+      <div className="w-full max-w-[820px] mx-auto flex items-center">
+        <a href="/" className="text-xl font-semibold mr-auto">
+          Simple Blog
+        </a>
+        <nav>
+          <a href="/login" className="font-medium">
+            Log In
+          </a>
+        </nav>
       </div>
     </header>
   );
