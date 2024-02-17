772ba9e2bdbe156c14d930f37cd3588b0cdd9002
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 11:58:52 +0700

    Add app router using react-router-dom

---
+++ src/router/Router.jsx
@@ -0,0 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import Header from "../components/Header";
+import PostsList from "../components/PostsList";
+import LoginForm from "../components/LoginForm";
+import PostDetail from "../components/PostDetail";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "/", element: <PostsList /> },
+        { path: "/login", element: <LoginForm /> },
+        { path: "/posts/:post_id", element: <PostDetail /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
+};
+
+export default Router;
