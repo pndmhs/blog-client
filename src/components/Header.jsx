30ce408f25351cee0c658c20d445036f19441ce4
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 12:09:27 +0700

    Change home link to be the children of h1 element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ const Header = () => {
   return (
     <header className="w-full flex items-center py-4 lg:py-6 px-5">
       <div className="w-full max-w-[820px] mx-auto flex items-center">
-        <a href="/" className="text-xl font-semibold mr-auto">
-          Simple Blog
-        </a>
+        <h1 className="text-xl font-semibold mr-auto">
+          <a href="/">Simple Blog</a>
+        </h1>
         <nav>
           <a href="/login" className="font-medium">
             Log In
