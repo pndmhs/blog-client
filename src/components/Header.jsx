6007caca8e04a49fe702e4010a8f3fa510760159
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-18 20:10:33 +0700

    Remove class from link component in h1 tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,7 @@ const Header = () => {
     <header className="w-full flex items-center py-4 lg:py-6 px-5">
       <div className="w-full max-w-[820px] mx-auto flex items-center">
         <h1 className="text-xl font-semibold mr-auto">
-          <Link to="/" className="font-medium">
-            Simple Blog
-          </Link>
+          <Link to="/">Simple Blog</Link>
         </h1>
         <nav>
           <Link to="/login" className="font-medium">
