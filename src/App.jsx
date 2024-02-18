b6cdbb23e5f945c6606dca2806b71c64593bd5af
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-18 21:26:44 +0700

    Add UserProvider component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import "@fontsource/merriweather";
 import PostsList from "./components/PostsList";
 import PostDetail from "./components/PostDetail";
 import LoginForm from "./components/LoginForm";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Header />
       <Routes>
         <Route path="/" element={<PostsList />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/posts/:post_id" element={<PostDetail />} />
       </Routes>
-    </>
+    </UserProvider>
   );
 }
 
