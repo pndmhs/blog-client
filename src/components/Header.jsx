283a805020c0de46456b24ddcd13c043a5ffab69
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-18 21:27:35 +0700

    Add logic to render the navigation based on user data from UserContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 const Header = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
   return (
     <header className="w-full flex items-center py-4 lg:py-6 px-5">
       <div className="w-full max-w-[820px] mx-auto flex items-center">
@@ -8,9 +17,18 @@ const Header = () => {
           <Link to="/">Simple Blog</Link>
         </h1>
         <nav>
-          <Link to="/login" className="font-medium">
-            Login
-          </Link>
+          {user ? (
+            <button
+              className="font-medium cursor-pointer"
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
+          ) : (
+            <Link to="/login" className="font-medium">
+              Login
+            </Link>
+          )}
         </nav>
       </div>
     </header>
