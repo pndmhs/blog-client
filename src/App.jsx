06ca18279f67864e95f1afd043fde9f597743147
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-18 20:07:06 +0700

    Change outlet into routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "@fontsource-variable/inter";
 import "@fontsource/merriweather";
+import PostsList from "./components/PostsList";
+import PostDetail from "./components/PostDetail";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/posts/:post_id" element={<PostDetail />} />
+      </Routes>
     </>
   );
 }
