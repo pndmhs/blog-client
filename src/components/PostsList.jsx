f3e45151313f6b0ce8fa0b2835bf75a8de2bf58d
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-19 09:15:48 +0700

    Add new post link based on auth status from AuthContext

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import PostItem from "./PostItem";
 import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const PostsList = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const { authed } = useContext(AuthContext);
 
   useEffect(() => {
     const getPosts = async () => {
@@ -29,9 +33,11 @@ const PostsList = () => {
       <div className="w-full max-w-[820px] mx-auto py-8">
         <div className="flex items-center">
           <h2 className="text-4xl font-semibold">Posts</h2>
-          <Link to="/posts/new" className="ml-auto">
-            + New Post
-          </Link>
+          {authed && (
+            <Link to="/posts/new" className="ml-auto">
+              + New Post
+            </Link>
+          )}
         </div>
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
