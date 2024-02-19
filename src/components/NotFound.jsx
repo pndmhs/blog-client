b92ab9aadb64a189fcb08d7c5a5fa2c0c738cace
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-19 19:40:31 +0700

    Create NotFound component

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <main className="w-full px-5">
+      <div className="w-full max-w-[820px] mx-auto py-8">
+        <h2 className="text-2xl font-semibold text-center">
+          Sorry, the page you're looking doesn't exist
+        </h2>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
