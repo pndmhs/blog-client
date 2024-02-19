eac9f0c31fcb843b223950ff22d8b96526bca9b4
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-19 21:06:02 +0700

    Add logic to add new post and send it to the API

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const NewPost = () => {
+  const [formData, setFormData] = useState();
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = JSON.parse(localStorage.getItem("user"));
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/posts/",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+      navigate(`/posts/${response.data._id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <main className="w-full px-5">
       <div className="w-full max-w-[820px] mx-auto py-8">
         <h2 className="text-4xl font-semibold mb-7">Add New Post</h2>
-        <form className="flex flex-col gap-5">
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-1">
             <label htmlFor="title">Title</label>
             <input
               type="text"
               name="title"
               className="border border-neutral-400 max-w-full py-1 px-2 outline-none focus:border-black"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col gap-1">
@@ -19,6 +51,7 @@ const NewPost = () => {
               cols="30"
               rows="10"
               className="border border-neutral-400 max-w-full py-1 px-2 outline-none focus:border-black"
+              onChange={handleChange}
             ></textarea>
           </div>
           <button
