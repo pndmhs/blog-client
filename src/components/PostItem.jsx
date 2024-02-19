77215d55f1837406bbdbb146b29b8d79c07cd4f5
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-19 21:04:22 +0700

    Add id prop and link component to the post url using id prop

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,18 +1,20 @@
 import LinesEllipsis from "react-lines-ellipsis";
+import { Link } from "react-router-dom";
 
-const PostItem = ({ title, text, date }) => {
+const PostItem = ({ id, title, text, date }) => {
   return (
     <div className="flex flex-col gap-2 cursor-pointer">
-      <h3 className="font-semibold text-xl hover:underline">{title}</h3>
-      <p className="font-merriweather text-sm opacity-80">
-        <LinesEllipsis
-          text={text}
-          maxLine="3"
-          ellipsis="..."
-          trimRight
-          basedOn="words"
-        />
-      </p>
+      <h3 className="font-semibold text-xl hover:underline">
+        <Link to={`/posts/${id}`}>{title}</Link>
+      </h3>
+      <LinesEllipsis
+        text={text}
+        maxLine="3"
+        ellipsis="..."
+        trimRight
+        basedOn="words"
+        className="font-merriweather text-sm opacity-80"
+      />
       <p className="text-yellow-900 text-sm">{date}</p>
     </div>
   );
