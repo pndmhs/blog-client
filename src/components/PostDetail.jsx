0a56e5690cc5dfe982364a1dded4362091f54d04
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-20 21:48:13 +0700

    Add edit and delete button

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,15 +1,23 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { DateTime } from "luxon";
 import Comments from "./Comments";
+import EditIcon from "../assets/edit.svg?react";
+import DeleteIcon from "../assets/trash.svg?react";
+import DeleteModal from "./DeleteModal";
+import { AuthContext } from "../context/AuthContext";
 
 const PostDetail = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const [deleteModal, setDeleteModal] = useState(false);
+
   const { post_id } = useParams();
+
+  const { authed } = useContext(AuthContext);
 
   useEffect(() => {
     const getPost = async () => {
@@ -33,6 +41,10 @@ const PostDetail = () => {
     return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_MED);
   };
 
+  const handleDeleteToggle = () => {
+    setDeleteModal(!deleteModal);
+  };
+
   return (
     <main className="w-full px-5">
       <div className="w-full max-w-[820px] mx-auto py-8">
@@ -42,6 +54,25 @@ const PostDetail = () => {
           <>
             <h2 className="font-semibold text-3xl mb-2">{data.title}</h2>
             <p className="text-yellow-900">{formatDate(data.created_at)}</p>
+            {authed && (
+              <div className="flex gap-4 mt-3">
+                <Link
+                  to={`/posts/${post_id}/edit`}
+                  className="flex gap-1 px-2 py-1 text-white bg-gray-900 rounded-md"
+                >
+                  <EditIcon />
+                  Edit Post
+                </Link>
+                <button
+                  className="flex gap-1 px-2 py-1 text-white bg-red-700 cursor-pointer rounded-md"
+                  onClick={handleDeleteToggle}
+                >
+                  <DeleteIcon />
+                  Delete Post
+                </button>
+              </div>
+            )}
+
             <div className="py-8 flex flex-col gap-6 font-merriweather">
               {data.text
                 .split("\n")
@@ -55,6 +86,14 @@ const PostDetail = () => {
           </>
         )}
       </div>
+      {deleteModal && (
+        <DeleteModal
+          post_id={post_id}
+          closeModal={() => {
+            setDeleteModal(false);
+          }}
+        />
+      )}
     </main>
   );
 };
