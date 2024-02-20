f49fa09875edd453f3b4f0cf33e8c50baa2332bc
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-20 09:50:27 +0700

    Add format date function

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,8 +1,14 @@
+import { DateTime } from "luxon";
+
 const CommentItem = ({ name, timestamp, text }) => {
+  const formatDate = (date) => {
+    return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_MED);
+  };
+
   return (
     <div className="py-4 px-5 border border-solid border-neutral-400">
       <h4 className="font-medium text-lg">{name}</h4>
-      <p className="text-yellow-900 text-sm">{timestamp}</p>
+      <p className="text-yellow-900 text-sm">{formatDate(timestamp)}</p>
       <p className="mt-3">{text}</p>
     </div>
   );
