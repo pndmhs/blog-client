070d3fa0312347fc21d361292f9b1deb65c44a1e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-20 09:49:39 +0700

    Move the state and effect into Comment component

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,51 +1,6 @@
-import { useEffect, useState } from "react";
 import CommentItem from "./CommentItem";
-import axios from "axios";
-import { useParams } from "react-router-dom";
 
-// const comments = [
-//   {
-//     name: "Enzo Fernandez",
-//     text: "Hello Admin!",
-//     timestamp: "12 February 2024, at 10:25",
-//   },
-//   {
-//     name: "Cole Palmer",
-//     text: "Great post!",
-//     timestamp: "12 February 2024, at 11.35",
-//   },
-//   {
-//     name: "Thiago Silva",
-//     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla urna porttitor rhoncus dolor. Non diam phasellus vestibulum lorem sed.",
-//     timestamp: "13 February 2024, at 11.45",
-//   },
-// ];
-
-const CommentLists = () => {
-  const [comments, setComments] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const { post_id } = useParams();
-
-  useEffect(() => {
-    const getComments = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/posts/${post_id}/comments`
-        );
-        setComments(response.data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setComments(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getComments();
-  }, []);
-
+const CommentsList = ({ comments, loading, error }) => {
   return (
     <>
       {loading && (
@@ -65,7 +20,7 @@ const CommentLists = () => {
             {comments.map((comment, index) => (
               <CommentItem
                 key={index}
-                name={comment.name}
+                name={comment.username}
                 timestamp={comment.timestamp}
                 text={comment.text}
               />
@@ -77,4 +32,4 @@ const CommentLists = () => {
   );
 };
 
-export default CommentLists;
+export default CommentsList;
