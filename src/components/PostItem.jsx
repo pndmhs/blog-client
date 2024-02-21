e38051a428b6070f9b80c9bc0e9dbcb77a4234ce
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-21 08:32:39 +0700

    Change all props with one data prop

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,21 +1,28 @@
 import LinesEllipsis from "react-lines-ellipsis";
 import { Link } from "react-router-dom";
+import { DateTime } from "luxon";
 
-const PostItem = ({ id, title, text, date }) => {
+const PostItem = ({ data }) => {
+  const formatDate = (date) => {
+    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
+  };
+
   return (
     <div className="flex flex-col gap-2 cursor-pointer">
       <h3 className="font-semibold text-xl hover:underline">
-        <Link to={`/posts/${id}`}>{title}</Link>
+        <Link to={`/posts/${data._id}`}>
+          {data.title} {!data.published && "(Not Published)"}
+        </Link>
       </h3>
       <LinesEllipsis
-        text={text}
+        text={data.text}
         maxLine="3"
         ellipsis="..."
         trimRight
         basedOn="words"
         className="font-merriweather text-sm opacity-80"
       />
-      <p className="text-yellow-900 text-sm">{date}</p>
+      <p className="text-yellow-900 text-sm">{formatDate(data.created_at)}</p>
     </div>
   );
 };
