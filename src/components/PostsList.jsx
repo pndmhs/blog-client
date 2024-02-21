e53d78ed3d5cecb579b4d1e315007a12cdda8ae4
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-21 08:31:41 +0700

    Change fetch data function to store the posts based on the authed state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -11,17 +11,14 @@ const PostsList = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const formatDate = (date) => {
-    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
-  };
-
   const { authed } = useContext(AuthContext);
 
   useEffect(() => {
     const getPosts = async () => {
       try {
         const response = await axios.get(`http://localhost:3000/posts`);
-        setData(response.data);
+        const publishedPosts = response.data.filter((post) => post.published);
+        setData(authed ? response.data : publishedPosts);
         setError(null);
       } catch (err) {
         setError(err.message);
@@ -31,7 +28,7 @@ const PostsList = () => {
       }
     };
     getPosts();
-  }, []);
+  }, [authed]);
 
   return (
     <main className="w-full px-5">
@@ -51,13 +48,7 @@ const PostsList = () => {
             <p className="text-yellow-900 mb-11">{data.length} Posts</p>
             <div className="flex flex-col gap-11">
               {data.map((post, index) => (
-                <PostItem
-                  key={index}
-                  title={post.title}
-                  text={post.text}
-                  date={formatDate(post.created_at)}
-                  id={post._id}
-                />
+                <PostItem key={index} data={post} />
               ))}
             </div>
           </>
