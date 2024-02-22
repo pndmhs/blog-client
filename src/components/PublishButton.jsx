a909472d565f956067dbdef2586ead9eb05c6560
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-22 08:57:08 +0700

    Add publish button that renders the text based on published status

---
+++ src/components/PublishButton.jsx
@@ -0,0 +1,25 @@
+import PublishIcon from "../assets/publish.svg?react";
+import UnpublishIcon from "../assets/unpublish.svg?react";
+
+const PublishButton = ({ published, handlePublishToggle }) => {
+  return (
+    <button
+      className="flex gap-2 px-3 py-2 text-white bg-gray-900 cursor-pointer rounded-md"
+      onClick={handlePublishToggle}
+    >
+      {!published ? (
+        <>
+          <PublishIcon />
+          Publish Post
+        </>
+      ) : (
+        <>
+          <UnpublishIcon />
+          Unpublish Post
+        </>
+      )}
+    </button>
+  );
+};
+
+export default PublishButton;
