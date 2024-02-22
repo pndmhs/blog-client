baab02672d0e5a568ac61b03f77dfa77010ab2c5
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-22 13:49:41 +0700

    Add delete comment function

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -11,27 +11,50 @@ const Comments = () => {
 
   const { post_id } = useParams();
 
+  const getComments = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/posts/${post_id}/comments`
+      );
+      setComments(response.data);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setComments(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteComment = async (comment_id) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    try {
+      await axios.delete(
+        `http://localhost:3000/posts/${post_id}/comments/${comment_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+      getComments();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    const getComments = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/posts/${post_id}/comments`
-        );
-        setComments(response.data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setComments(null);
-      } finally {
-        setLoading(false);
-      }
-    };
     getComments();
   }, []);
 
   return (
     <>
-      <CommentsList comments={comments} loading={loading} error={error} />
+      <CommentsList
+        comments={comments}
+        loading={loading}
+        error={error}
+        deleteComment={deleteComment}
+      />
       <CommentForm comments={comments} setComments={setComments} />
     </>
   );
