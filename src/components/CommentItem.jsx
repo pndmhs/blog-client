83b5b6ce47f842cade6c90ca66b64fb9eacc2cd0
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-22 13:51:43 +0700

    Change comment data props into single data prop and add deleteComment prop

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,9 +1,8 @@
 import { DateTime } from "luxon";
-import DeleteIcon from "../assets/trash.svg?react";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
-const CommentItem = ({ name, timestamp, text }) => {
+import DeleteCommentButton from "./DeleteCommentButton";
+const CommentItem = ({ data, deleteComment }) => {
   const { authed } = useContext(AuthContext);
 
   const formatDate = (date) => {
@@ -13,14 +12,12 @@ const CommentItem = ({ name, timestamp, text }) => {
   return (
     <div className="py-4 px-5 border border-solid border-neutral-400 rounded-md flex">
       <div className="mr-auto">
-        <h4 className="font-medium text-lg">{name}</h4>
-        <p className="text-yellow-900 text-sm">{formatDate(timestamp)}</p>
-        <p className="mt-3">{text}</p>
+        <h4 className="font-medium text-lg">{data.username}</h4>
+        <p className="text-yellow-900 text-sm">{formatDate(data.timestamp)}</p>
+        <p className="mt-3">{data.text}</p>
       </div>
       {authed && (
-        <button className="bg-red-700 px-1 py-1 text-white rounded-md cursor-pointer self-start">
-          <DeleteIcon />
-        </button>
+        <DeleteCommentButton deleteComment={() => deleteComment(data._id)} />
       )}
     </div>
   );
