abe59e01bc376fc6f905f322b85ade11a9b1aadb
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-24 23:02:10 +0700

    Change login error handling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
+import InfoLogo from "../assets/info-circle.svg?react";
 import { AuthContext } from "../context/AuthContext";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
   const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
@@ -18,9 +20,15 @@ const LoginForm = () => {
     e.preventDefault();
     try {
       await login(formData);
+      setError(null);
       navigate("/");
     } catch (err) {
-      console.log(err.message);
+      console.log(err);
+      if (err.response.status === 401) {
+        setError("Incorrect username or password");
+      } else {
+        setError(err.message);
+      }
     }
   };
 
@@ -29,6 +37,12 @@ const LoginForm = () => {
       <div className="w-full max-w-96 my-8 mx-auto flex flex-col items-center px-10 py-8">
         <h2 className="text-3xl font-semibold mb-2">Welcome Back!</h2>
         <p className="mb-10">Please log in to your account</p>
+        {error && (
+          <div className="mb-5 text-red-600 flex items-center gap-2">
+            <InfoLogo width="1rem" height="1rem" />
+            <p>{error}</p>
+          </div>
+        )}
         <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full">
           <div className="flex flex-col gap-2">
             <input
